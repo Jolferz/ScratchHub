d7702488241fdc5e43d1861875ebda3c96e7b379
Jose Lora <jlora018@gmail.com>
2017-05-08 02:10:03 -0400

    upload progress bar

--- public/js/upload.js
+++ public/js/upload.js
@@ -32,24 +32,24 @@ $('#upload-input').on('change', function(){
       },
       xhr: function() {
         // create an XMLHttpRequest
-        var xhr = new XMLHttpRequest()
+        let xhr = new XMLHttpRequest()
 
         // listen to the 'progress' event
         xhr.upload.addEventListener('progress', function(evt) {
 
           if (evt.lengthComputable) {
-            // calculate the percentage of upload completed
-            var percentComplete = evt.loaded / evt.total
-            percentComplete = parseInt(percentComplete * 100)
+			// calculate the percentage of upload completed
+			let percentComplete = evt.loaded / evt.total
+			percentComplete = parseInt(percentComplete * 100)
 
-            // update the Bootstrap progress bar with the new percentage
-            $('.progress-bar').text(percentComplete + '%')
-            $('.progress-bar').width(percentComplete + '%')
+			// update the Bootstrap progress bar with the new percentage
+			$('.progress-bar').text(percentComplete + '%')
+			$('.progress-bar').width(percentComplete + '%')
 
-            // once the upload reaches 100%, set the progress bar text to done
-            if (percentComplete === 100) {
-              $('.progress-bar').html('Done')
-            }
+			// once the upload reaches 100%, set the progress bar text to done
+			if (percentComplete === 100) {
+			  $('.progress-bar').html('Done')
+			}
 
           }
 
